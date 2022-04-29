ddd99518f144185e58e2d8c2581a76cbbcba44f5
Pierre Deveix <deveixpierre@gmail.com>
2022-04-29 20:41:21 +0200

    testing routes and updating server.js

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('requête reçue!');
+  next();
+});
+
 app.use((req, res) => {
   res.json({ message: 'Message reçu !'})
 });
+
+
 
 module.exports = app;
