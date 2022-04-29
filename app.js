23b93129a4543e9435cb58a5d3d21a1e72909b53
Pierre Deveix <deveixpierre@gmail.com>
2022-04-29 21:05:05 +0200

    adding mongoose and mongoDB

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://pierredeveix:panoramata@sidran83.5hbgs.mongodb.net/piiquante?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 // give access to req.body
 app.use(express.json());
@@ -14,24 +21,24 @@ app.use((req, res, next) => {
 });
 
 // test routes
-app.post('/api/stuff', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({ message: 'Objet créé !' });
-  next();
-});
+// app.post('/api/stuff', (req, res, next) => {
+//   console.log(req.body);
+//   res.status(201).json({ message: 'Objet créé !' });
+//   next();
+// });
 
-app.get('/api/stuff', (req, res, next) => {
-  const stuff = [
-    {
-      _id: 'oeihfzeoi',
-      title: 'Mon premier objet',
-    },
-    {
-      _id: 'oeihfzeomoihi',
-      title: 'Mon deuxième objet',
-    },
-  ];
-  res.status(200).json(stuff);
-});
+// app.get('/api/stuff', (req, res, next) => {
+//   const stuff = [
+//     {
+//       _id: 'oeihfzeoi',
+//       title: 'Mon premier objet',
+//     },
+//     {
+//       _id: 'oeihfzeomoihi',
+//       title: 'Mon deuxième objet',
+//     },
+//   ];
+//   res.status(200).json(stuff);
+// });
 
 module.exports = app;
