1955cd9e970ca68c8a8e9c5f26ec841a91bd984a
Pierre Deveix <deveixpierre@gmail.com>
2022-04-30 14:55:18 +0200

    changing modifySauce function to handle file with multer

--- routes/sauce.js
+++ routes/sauce.js
@@ -11,7 +11,7 @@ const multer = require('../middleware/multer-config');
 router.post('/', auth, multer, saucesCtrl.createSauce);
 router.get('/', auth, saucesCtrl.getAllSauces);
 router.get('/:id', auth, saucesCtrl.getOneSauce);
-router.put('/:id', auth, saucesCtrl.modifySauce);
+router.put('/:id', auth, multer, saucesCtrl.modifySauce);
 router.delete('/:id', auth, saucesCtrl.deleteSauce);
 
 module.exports = router;
