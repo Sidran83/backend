61220f3b0760fc09c4b431a3633bf48fc459889f
Pierre Deveix <deveixpierre@gmail.com>
2022-04-30 18:16:57 +0200

    generate long token and hide it in dotenv file

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,12 @@
 const jwt = require('jsonwebtoken');
 
+// importer dotenv pour cacher le token d'authentification
+require('dotenv').config({ path: '../.env' })
+
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
     const userId = decodedToken.userId;
     req.auth = { userId };
     if (req.body.userId && req.body.userId !== userId ) {
