93e0d9a539c6c3e4f0d2e27b09f66539bfb0088f
Pierre Deveix <deveixpierre@gmail.com>
2022-04-30 11:30:29 +0200

    function login implemented and tested

--- controllers/user.js
+++ controllers/user.js
@@ -16,7 +16,22 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-  User.find()
-    .then(users => res.status(200).json(users))
-    .catch(error => res.status(400).json({ error }));
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({ error : 'Utilisateur non trouvé !' });
+      }
+      bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+          if (!valid) {
+            return res.status(401).json({ error : 'Mot de passe incorrect !' });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: 'TOKEN'
+          });
+        })
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
 };
