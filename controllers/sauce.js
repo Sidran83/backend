ae2502964762be43542840261c16f340d59cbaa3
Pierre Deveix <deveixpierre@gmail.com>
2022-04-30 17:08:11 +0200

    adding likeSauce function to sauce controller

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -52,3 +52,29 @@ exports.deleteSauce = (req, res, next) => {
     })
     .catch(error => res.status(500).json({ error }));
 };
+
+exports.likeSauce = (req, res, next) => {
+  if (req.body.like === 1) {
+    Sauce.updateOne({ _id: req.params.id }, { $inc: { likes: 1 }, $push: { usersLiked: req.body.userId }})
+      .then(() => res.status(200).json({ message: 'Like ajouté !' }))
+      .catch((error) => res.status(400).json({ error }));
+  } else if (req.body.like === -1) {
+    Sauce.updateOne({ _id: req.params.id }, { $inc: { dislikes: 1 }, $push: { usersDisliked: req.body.userId }})
+      .then(() => res.status(200).json({ message: 'Dislike ajouté !' }))
+      .catch((error) => res.status(400).json({ error }));
+  } else {
+    Sauce.findOne({ _id: req.params.id })
+      .then((sauce) => {
+        if (sauce.usersLiked.includes(req.body.userId)) {
+          Sauce.updateOne({ _id: req.params.id }, { $inc: { likes: -1 }, $pull: { usersLiked: req.body.userId }})
+            .then(() => res.status(200).json({ message: 'Like supprimé !' }))
+            .catch((error) => res.status(400).json({ error }));
+        } else if (sauce.usersDisliked.includes(req.body.userId)){
+          Sauce.updateOne({ _id: req.params.id }, { $inc: { dislikes: -1 }, $pull: { usersDisliked: req.body.userId }})
+            .then(() => res.status(200).json({ message: 'Dislike supprimé !' }))
+            .catch((error) => res.status(400).json({ error }));
+        }}
+      )
+      .catch((error) => res.status(400).json({ error }));
+  }
+};
